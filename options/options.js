a06baabfe27e30ecb38978bbe3bd81d8a7eaa47e
Crowdbotics <team@crowdbotics.com>
2023-05-23 23:15:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pharma-wiki-40940",
+  url: "https://pharma-wiki-40940.botics.co",
+  api: "https://pharma-wiki-40940.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
